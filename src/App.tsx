74c9d0f6984d83d46cc0c335c63653b80a329b65
Ms-Walsbee <kursten.massey@maine.edu>
2025-03-04 17:31:38 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App(): React.JSX.Element {
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
+                Kursten Massey
             </p>
         </div>
     );
